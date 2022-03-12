cf312d79603b9200d92973549adb078ca61b19bd
Giorgos Dimitriou <giwrgosdi@gmail.com>
2022-03-12 11:06:35 +0000

    Route request through an interface

--- main.go
+++ main.go
@@ -1,37 +1,22 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"github.com/giorgosdi/kubectl-kopy/pkg/secret"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	coreV1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 type resource interface {
-	removeGen()
-}
-
-type secret struct {
-	o coreV1.Secret
-	n coreV1.Secret
-}
-
-func (s *secret) removeGen() {
-	secretKopy := s.o.DeepCopy()
-	objMeta(&secretKopy.ObjectMeta)
-	s.n = *secretKopy
+	Kopy()
 }
 
 type res struct {
 	r resource
 }
 
-func (resource res) removeGen() {
-	resource.r.removeGen()
+func (resource res) Kopy() {
+	resource.r.Kopy()
 }
 
 func GetClients(conf string) *kubernetes.Clientset {
@@ -41,34 +26,13 @@ func GetClients(conf string) *kubernetes.Clientset {
 	return resourceClient
 }
 
-func objMeta(meta *metav1.ObjectMeta) {
-	newAnnotations := removeAnnotations(meta.Annotations)
-	emptyManagedFields := []metav1.ManagedFieldsEntry{}
-	meta.SetSelfLink("")
-	meta.SetResourceVersion("")
-	meta.SetUID("")
-	meta.SetCreationTimestamp(metav1.Time{})
-	meta.SetManagedFields(emptyManagedFields)
-	meta.SetAnnotations(newAnnotations)
-}
-
-func removeAnnotations(annotations map[string]string) map[string]string {
-	delete(annotations, "kubectl.kubernetes.io/last-applied-configuration")
-	delete(annotations, "hash")
-	delete(annotations, "fluxcd.io/sync-checksum")
-	return annotations
-}
-
 func main() {
 	clientset := GetClients("/Users/giorgos.dimitriou/.kube/legacy")
-	secretsClient := clientset.CoreV1().Secrets("monitoring")
+	s := secret.Secret{}
+	s.Client = *clientset
 
-	s, err := secretsClient.Get(context.TODO(), "logstash-newrelic", metav1.GetOptions{})
-	if err != nil {
-		fmt.Println(err)
-	}
-	sec := secret{o: *s}
-
-	r := res{r: &sec}
-	r.removeGen()
+	result := s.GetSecret("monitoring", "logstash-newrelic")
+	s.O = *result
+	rs := res{r: &s}
+	rs.Kopy()
 }
